0ac5f0478e270a1e2760c24852d161955d239685
DimaUbuntu <glooshak@gmail.com>
2021-04-14 07:41:21 +0300

    Added the validator which examinates whether a file with its hash is already exists in a storage.

--- files_manager/models.py
+++ files_manager/models.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
 from django.db.models.signals import pre_save
@@ -9,16 +10,23 @@ from .hashes import calculate_file_hash
 
 
 def submit_file_path(instance, filename):
-    file_hash = calculate_file_hash(instance.file)
+    file_hash = instance.file_hash
     sub_dir_name = file_hash[:2]
     return Path(sub_dir_name) / Path(file_hash)
 
 
+def validate_file(uploading_file):
+    # TODO a file is not on a disk, it is in memory, what will happen if file will be larger than capacity of memory?
+    file_hash = calculate_file_hash(uploading_file)
+    if Data.objects.filter(file_hash=file_hash).exists():
+        raise ValidationError(f'The file with this hash is already exists {file_hash}')
+    else:
+        validate_file.previous_file_hash = file_hash
+
+
 class Data(models.Model):
-    # TODO If there is a file with the already existed hash UNIQUE constraint failed will raise
-    # TODO need to handle this issue.
     file_hash = models.CharField(primary_key=True, max_length=64, default='')
-    file = models.FileField(upload_to=submit_file_path, null=True, max_length=255)
+    file = models.FileField(upload_to=submit_file_path, null=True, validators=[validate_file], max_length=255)
     date_created = models.DateTimeField(default=timezone.now)
 
     class Meta:
@@ -31,4 +39,4 @@ class Data(models.Model):
 
 @receiver(signal=pre_save, sender=Data)
 def assign_pk(instance, **kwargs):
-    instance.file_hash = calculate_file_hash(instance.file)
+    instance.file_hash = validate_file.previous_file_hash
