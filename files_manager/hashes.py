173b39003d986b7006d520c39adfedd6a737b904
DimaUbuntu <glooshak@gmail.com>
2021-04-16 23:01:35 +0300

    Corrected spelling mistakes.

--- files_manager/hashes.py
+++ files_manager/hashes.py
@@ -7,9 +7,9 @@ def calculate_file_hash(file: FieldFile) -> str:
     # Together the MemoryFileUploadHandler and TemporaryFileUploadHandler
     # provide Django’s default file upload behavior of reading small files
     # into memory and large ones onto disk.
-    # If a file is large file will be a instance of TemporaryUploadedFile,
+    # If a file is large file will be an instance of TemporaryUploadedFile,
     # TemporaryUploadedFile.temporary_file_path() returns the full path to the temporary uploaded file.
-    # If a file is small file will be a instance of InMemoryUploadedFile.
+    # If a file is small file will be an instance of InMemoryUploadedFile.
     file_hash = sha256()
     file_buffer = file.read(file.DEFAULT_CHUNK_SIZE)
     while len(file_buffer) > 0:
