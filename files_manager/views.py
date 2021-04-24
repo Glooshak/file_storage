bb4e4f6f4da17d113eb4d1c8da5becaedb737841
DimaUbuntu <glooshak@gmail.com>
2021-04-24 12:20:02 +0300

    Fixed the issue has to do with fetching the freshest record from the db.

--- files_manager/views.py
+++ files_manager/views.py
@@ -65,7 +65,7 @@ class DataViewSet(viewsets.ModelViewSet):
             headers = self.get_success_headers(serializer.data)
             serializer.data.pop('file')
             return Response(
-                data={'file_hash': Data.objects.last().file_hash},
+                data={'file_hash': Data.objects.first().file_hash},
                 status=status.HTTP_201_CREATED,
                 headers=headers
             )
@@ -73,7 +73,7 @@ class DataViewSet(viewsets.ModelViewSet):
             logger.warning(f'Somebody deleted a file that has its record in the db,'
                            f' but another file with the same hash was uploaded, so this new file '
                            f'will inherit the record in the db of the old deleted file.')
-            return Response(data={'file_hash': Data.objects.last().file_hash}, status=status.HTTP_201_CREATED)
+            return Response(data={'file_hash': Data.objects.first().file_hash}, status=status.HTTP_201_CREATED)
 
     def destroy(self, request, *args, **kwargs):
         try:
